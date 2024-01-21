22f0f89bf117462e8e50ed46ba28ee20e5618080
JW148 <47108195+JW148@users.noreply.github.com>
2024-01-21 19:28:08 +0000

    Created seed data from exisiting events table data

--- app/ui/navbar/navbar.js
+++ app/ui/navbar/navbar.js
@@ -3,8 +3,8 @@ import NavLinks from "@/app/ui/navbar/nav-links";
 export default function NavBar() {
   return (
     <div className="flex bg-slate-400">
-      <div className="flex-none w-14 h-14 bg-slate-800">
-        <p>Logo</p>
+      <div className="flex-none w-14 h-14 bg-slate-800 text-white">
+        <p className="text-center">Logo</p>
       </div>
       <div className="flex-auto"></div>
       <div className="flex">
